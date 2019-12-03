155504940c4a9152985fe73d520a46f7ff57bc0a
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-03 08:43:55 +0545

    login and register activity added.

---
+++ app/src/main/java/com/yagya/easylaundry/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.yagya.easylaundry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
