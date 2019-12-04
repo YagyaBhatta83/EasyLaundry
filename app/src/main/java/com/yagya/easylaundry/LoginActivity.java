374cd4f9ccbae19d92c7c49073f4277c1cf53442
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-04 23:08:54 +0545

    login Sucessfully.

--- app/src/main/java/com/yagya/easylaundry/LoginActivity.java
+++ app/src/main/java/com/yagya/easylaundry/LoginActivity.java
@@ -1,14 +1,60 @@
 package com.yagya.easylaundry;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class LoginActivity extends AppCompatActivity {
+
+    private View findViewById;
+    public EditText emailid, password;
+    private Button loginBtn;
+    //    private TextView signUp;
+//    private CheckBox show_hide_password;
+//    private LinearLayout loginLayout;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailid = (EditText) findViewById(R.id.emailid);
+        password = (EditText) findViewById(R.id.password);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+//        signUp = (TextView) view.findViewById(R.id.createAccount);
+//        show_hide_password = (CheckBox) view
+//                .findViewById(R.id.show_hide_password);
+//        loginLayout = (LinearLayout) view.findViewById(R.id.login_layout);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Emailid = emailid.getText().toString();
+                String Password = password.getText().toString();
+
+                if (Emailid.isEmpty()) {
+                    emailid.setError("Enter Username!");
+                } else if (Password.isEmpty()) {
+                    password.setError("Enter Password!");
+                } else {
+                    validate(emailid.getText().toString(), password.getText().toString());
+                }
+                validate(emailid.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String emailid, String password) {
+        if ((emailid.equals("admin")) && (password.equals("admin123"))) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+//        else {
+//            emailid.setError("Please Enter Email and Password");
+//        }
     }
 }
--- app/src/main/java/com/yagya/easylaundry/RegisterActivity.java
+++ app/src/main/java/com/yagya/easylaundry/RegisterActivity.java
@@ -1,14 +1,30 @@
 package com.yagya.easylaundry;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class RegisterActivity extends AppCompatActivity {
+
+//    private static View view;
+//    private static EditText fullName, emailId, mobileNumber, location,
+//            password, confirmPassword;
+//    private static TextView login;
+//    private static Button signUpButton;
+//    private static CheckBox terms_conditions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+//        fullName = (EditText) view.findViewById(R.id.fullName);
+//        emailId = (EditText) view.findViewById(R.id.userEmailId);
+//        mobileNumber = (EditText) view.findViewById(R.id.mobileNumber);
+//        location = (EditText) view.findViewById(R.id.location);
+//        password = (EditText) view.findViewById(R.id.password);
+//        confirmPassword = (EditText) view.findViewById(R.id.confirmPassword);
+//        signUpButton = (Button) view.findViewById(R.id.signUpBtn);
+//        login = (TextView) view.findViewById(R.id.already_user);
+//        terms_conditions = (CheckBox) view.findViewById(R.id.terms_conditions);
     }
 }
